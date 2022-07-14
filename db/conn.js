d1e4b08c38e412b602a8f3ba2ba8b7ee26ded465
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-14 19:09:06 -0300

    Instalação do Mongoose e Refactoring da conexão coma base de dados.

--- db/conn.js
+++ db/conn.js
@@ -1,20 +1,11 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/node-mongo";
-const client = new MongoClient(uri);
-
-async function run() {
-    try {
-
-        await client.connect();
-        console.log('Connected to MongoDB');
-        const db = client.db('node-mongo');
-
-    } catch(err) {
-        console.log(err);
-    }
+async function main() {
+    const conn = await mongoose.createConnection('mongodb://localhost:27017/node-mongo');
+    
+    console.log('Connected to MongoDB with Mongoose');
 }
 
-run();
+main().catch(err => console.error(err));
 
-module.exports = client;
+module.exports = mongoose;
